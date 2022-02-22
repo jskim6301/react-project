a40043830ab2c9f15bff67f6524b4b762e6a0989
jskim6301 <jskim6301@naver.com>
2022-02-22 18:09:33 +0900

    redux 라이브러리 사용

---
+++ Library/redux/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import TestMocking from './components/TestMocking';
+import Counter2 from './components/Counter2';
+import Counter from './features/counter/Counter';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TestMocking/>
+      <Counter />
+      <br/>
+      <Counter2/>
+    </div>
+  );
+}
+
+export default App;
