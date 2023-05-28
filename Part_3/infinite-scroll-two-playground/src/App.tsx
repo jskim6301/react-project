c4d19b9a34c6040bb8de2141889bce6195e55871
jskim <jskim6301@douzone.com>
2023-05-28 14:35:16 +0900

    무한스크롤#2

--- Part_3/infinite-scroll-two-playground/src/App.tsx
+++ Part_3/infinite-scroll-two-playground/src/App.tsx
@@ -1,25 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import useIntersectionObserver from './hooks/useIntersectionObserver';
+
+interface Airline {
+  id: number;
+  name: string;
+  country: string;
+  logo: string;
+  slogan: string;
+  head_quaters: string;
+  website: string;
+  established: string;
+}
+
+interface Passengers {
+  _id: string;
+  name: string;
+  trips: number;
+  airline: Airline;
+  __v: number;
+}
+
+interface Props {
+  isLastItem: boolean;
+  onFetchMorePassengers: () => void;
+  children?: any;
+}
+
+const Passenger: React.FC<Props> = ({children,isLastItem,onFetchMorePassengers}) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const entry = useIntersectionObserver(ref,{});
+  const isIntersecting = !!entry?.isIntersecting;
+
+  useEffect(() => {
+    isLastItem && isIntersecting && onFetchMorePassengers();
+  },[isLastItem,isIntersecting]);
+
+  return(
+    <div
+      ref={ref}
+      style={{
+        minHeight: '100vh',
+        display:'flex',
+        border:'1px dashed #000',
+      }}
+    >
+      <div style={{margin: 'auto'}}>{children}</div>
+    </div>
+  );
+}
 
 function App() {
+
+  const [passengers, setPassengers] = useState<Array<Passengers>>([]);
+  const [isLast, setIsLast] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(0);
+
+  const getPassengers = async () => {
+    // const page = currentPageRef.current;
+    const size = 30;
+    try {
+      
+      const response = await axios.get(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=${size}`);
+      const passenger = response.data.data;
+      console.log("passenger??",passenger);
+      // const isLast = response.data.totalPages === page;
+      const isLast = response.data.totalPages === page;
+
+      setPassengers(prev => [...prev,...passenger]);
+      setIsLast(isLast);
+
+    } catch (e) {
+      console.error(e);
+    }    
+  }
+
+  useEffect(() => {
+    !isLast && getPassengers();
+  },[page]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        passengers.map((passenger,idx) => (
+          <Passenger 
+          key={passenger._id}
+          isLastItem={passengers.length - 1 === idx}
+          onFetchMorePassengers={() => setPage(prev => prev + 1)}
+          >
+            {passenger.name}
+          </Passenger>
+        ))
+      }
+    </>
   );
 }
 
---
+++ Part_3/infinite-scroll-two-playground/src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,37 @@
+import React, { RefObject, useEffect, useState } from 'react';
+
+
+
+const useIntersectionObserver = (
+    targetRef: RefObject<Element>,
+    options: IntersectionObserverInit = {
+        threshold: 0,
+        root: null,
+        rootMargin: '0%',
+    },
+): IntersectionObserverEntry | undefined => {
+    const [entry, setEntry] = useState<IntersectionObserverEntry>();
+
+    const isIntersecting = entry?.isIntersecting;
+
+    const updateEntry = (entries: IntersectionObserverEntry[]): void => {
+        const [entry] = entries;
+        setEntry(entry);
+    }
+
+    useEffect(() => {
+        const target = targetRef.current;
+        if(isIntersecting || !target) return;
+
+        const observer = new IntersectionObserver(updateEntry,options);
+        observer.observe(target);
+
+        return () => {
+            observer.disconnect();
+        }
+    },[targetRef, options.root, options.rootMargin, options.threshold, isIntersecting]);
+
+    return entry;
+};
+
+export default useIntersectionObserver;
